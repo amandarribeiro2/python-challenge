762e6b0ec69d115448246d24c8a37daa3e48900c
Amanda Ribeiro <36522293+amandarribeiro2@users.noreply.github.com>
2018-03-14 10:02:23 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,56 +1,44 @@
-# Dependencies
- import csv
- import moment
- from datetime import datetime
- import random
- 
- # Create output file name
- file_output_name = "budget_data2.csv"
- 
- # Months of Financial Data (Change as needed)
- num_months = 85
- current_date = moment.date("01-01-2016", "M-D-YYYY")
- starting_date = current_date.add(months=-num_months)
- 
- # % of months that are positive vs negative (Change as needed)
- gain_loss_weights = [0.85, 0.15]
- 
- # Range of profits or losses (Chane as needed)
- range_revenue = [50000, 1200000]
- 
- # Iteratively build every month between start and current date
- tracked_months = []
- current_month = starting_date
- revenues = []
- 
- for x in range(0, num_months + 1):
- 
-     # Generate a list of formatted months
-     current_month = current_month.add(month=1)
-     current_month_string = current_month.date.strftime("%b-%Y")
-     tracked_months.append(current_month_string)
- 
-     # Generate a random revenue for each month
-     random_revenue = random.randrange(range_revenue[0], range_revenue[1])
-     revenues = revenues + [random_revenue]
- 
- # Convert a set amount of revenues to be negative
- neg_months = int(num_months * gain_loss_weights[1])
- 
- for x in range(neg_months):
-     revenues[x] = -1 * revenues[x]
- 
- # Shuffle the revenues so the negatives appear sporadically
- random.shuffle(revenues)
- 
- # Assemble the months and revenues into a single list
- fin_data = zip(tracked_months, revenues)
- 
- # Print the financial data to terminal
- print(fin_data)
- 
- # Export a csv of the generated financial data
- with open(file_output_name, "w", newline="") as datafile:
-     writer = csv.writer(datafile)
-     writer.writerow(["Date", "Revenue"])
-     writer.writerows(fin_data)
+import os
+import csv
+
+Data_1 = "Budget_data_1.csv"
+Data_2 = "Budget_data_2.csv"
+lastmonth_value = ["", 0];
+
+with open(Data_1, newline="", encoding = 'latin-1') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    month = 0
+    total_revenue = 0
+    increased_value = ["", + 0];
+    decreased_value = ["", + 0];
+
+    for row in csvreader:
+        #print(row[1])
+        month += 1
+        if month != 1:
+            total_revenue = total_revenue + int(row[1])
+            if lastmonth_value != 0 and (increased_value[1] < int(row[1]) - lastmonth_value[1]):
+                increased_value[0] = row [0]
+                increased_value[1] = int(row[1]) -lastmonth_value[1]
+                #print("Value Increased: " + str(increased_value[1]))
+       
+            # print(row[0])
+            if lastmonth_value != 0 and (decreased_value[1] > int(row[1]) - lastmonth_value[1]):
+                decreased_value[0] = row[0]
+                decreased_value[1] = int(row[1]) - lastmonth_value[1]
+                #print("Value Decreased: " + str(decreased_value[1]))
+            lastmonth_value[1] = int(row[1])
+            lastmonth_value[0] = row[0]
+
+print("Financial Analysis")
+print("------------------")
+
+print("Total Months: " + str(month - 1))
+
+print("Total Revenue: $" + str(total_revenue))
+
+print("Average Revenue Change: $" + str(total_revenue / (month -1)))
+
+print("Greatest Increase in Revenue: " + str(increased_value))
+
+print("Greatest Decrease in Revenue: " + str(decreased_value))
